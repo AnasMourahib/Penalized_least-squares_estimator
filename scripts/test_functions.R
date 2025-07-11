96f04cd3ade1e26a20206c28bd47802c6384ae5c
AnasMourahib <anas.mourahib93@gmail.com>
2025-07-11 15:15:25 +0200

    Here, we add the documentation of functions from Help_functions.R

--- scripts/test_functions.R
+++ scripts/test_functions.R
@@ -17,9 +17,9 @@ source("Functions/main.R")
 
 
 N <- 10^6
-d <- 4 
-r <- 4 
-num_col <- 4 
+d <- 4
+r <- 4
+num_col <- 4
 points <- c(0 , 1/6,  1/8 ,  1/4 ,  1/3 , 1/2 , 2/3 , 3/4 , 1)
 Grid_points_HR <- selectGrid(points, d = d, nonzero = 2  )
 lambda <- 10^(-3)
@@ -32,11 +32,11 @@ start <- starting_point(X, num_col)
 start <- c(t(start))
 p <- 0.3
 q <- nrow(Grid_points_HR)
-k <- 0.1 
+k <- 0.1
 w_total <- sapply(1:q, function(m) stdfEmp(R, k * N, Grid_points_HR[m, ]))
 
 
-#####Test for param_estim 
+#####Test for param_estim
 
 param_estim(d , r , A , grid = Grid_points_HR , lambda , num_col = NULL , start , type = "SSR_row_HR", p = p ,  w = w_total )
 
@@ -79,16 +79,16 @@ for(i in 1 : 1){
 
 
 
-#############Transform data 
+#############Transform data
 n <- 100
-list <- list() 
-count <- 0 
-for(i in 1: n){          
+list <- list()
+count <- 0
+for(i in 1: n){
   file_path <- paste0("C:/Users/mourahib/Desktop/github/Penalized_least-squares_estimator/Results/DA_mix_HR/extr_dir_ident/wrong_model/kn0.08/result_",i,".rds")
   if(file.exists(file_path)){
     count <- count + 1
     result <- readRDS(file_path)
-    list[[count]] <- result 
+    list[[count]] <- result
   }
   else(print(i))
 }
@@ -97,14 +97,14 @@ saveRDS(list , file= "C:/Users/mourahib/Desktop/github/Penalized_least-squares_e
 
 
 
-###Effect of the tail fraction on teh score EDS      
+###Effect of the tail fraction on teh score EDS
 n <- 100   #Replace p=0.2 by p=0.4 or p=0.6  4''
 list <- readRDS(file= paste0("C:/Users/mourahib/Desktop/github/Penalized_least-squares_estimator/Results/DA_mix_log/effect_tail_fraction/N3000/pen0.2.rds"))
 list[[1]] <- list[[2]]
-#Calculus of EDS score. 
+#Calculus of EDS score.
 EDscore_p0.2 <- rep(0 , 10)
 for( j in 1:10  ){
-  list_matrices <- list()  
+  list_matrices <- list()
   for(i in 1: n){
     list_matrices <- list[[i]][[j]]$Estimation$matrix
     scorei <- EDS( list_matrices , A)
@@ -117,15 +117,15 @@ for( j in 1:10  ){
 colors <- c("#E69F00", "#56B4E9", "#009E73", "#D55E00")  # Orange, Blue, Green
 
 # X-axis values
-x_values <- seq(0.02 , 0.2 , by=0.02) 
+x_values <- seq(0.02 , 0.2 , by=0.02)
 
 # Set up an empty plot with appropriate axis limits
 par(mgp = c(2, 0.7, 0))  # Adjust label position
-plot(x_values, EDscore_p0.2 , type = "l", col = colors[1], lty = 1, lwd = 2.5, 
-     ylim =  c(0.5 , 1), 
+plot(x_values, EDscore_p0.2 , type = "l", col = colors[1], lty = 1, lwd = 2.5,
+     ylim =  c(0.5 , 1),
      xlab = "",
-      ylab = "" , #  "ED-S", 
-     cex.lab = 1.5, cex.axis = 1.3, 
+      ylab = "" , #  "ED-S",
+     cex.lab = 1.5, cex.axis = 1.3,
      xaxt = "n")
 
 # Add custom x-axis ticks and labels
@@ -146,11 +146,11 @@ points(x_values, EDscore_p0.6 , col = colors[3], pch = 16, cex = 1.5)
 points(x_values, EDscore_p1 , col = colors[4], pch = 16, cex = 1.5)
 
 
-###Effect of the tail fraction on the score SMSE 
+###Effect of the tail fraction on the score SMSE
 n <- 100   #Replace p=0.2 by p=0.4 or p=0.6  4
 list <- readRDS(file= paste0("C:/Users/mourahib/Desktop/github/Penalized_least-squares_estimator/Results/DA_mix_log/effect_tail_fraction/N3000/pen1.rds"))
 list[[1]] <- list[[2]]
-#Calculus of EDS score. 
+#Calculus of EDS score.
 list_res <- list()
 SMSE_p1 <- rep(0 , 10)
 for( j in 1:10  ){
@@ -158,7 +158,7 @@ for( j in 1:10  ){
   for(i in 1 : n){
     list_res[[i]] <- list[[i]][[j]]$Estimation
   }
-  SMSE_p1[j] <- SMSE_log_2(list_res , A , dep = 0.25)
+  SMSE_p1[j] <- SMSE_log(list_res , A , dep = 0.25)
 }
 
 
@@ -167,15 +167,15 @@ for( j in 1:10  ){
 colors <- c("#E69F00", "#56B4E9", "#009E73", "#D55E00")  # Orange, Blue, Green
 
 # X-axis values
-x_values <- seq(0.02 , 0.2 , by=0.02) 
+x_values <- seq(0.02 , 0.2 , by=0.02)
 
 # Set up an empty plot with appropriate axis limits
 par(mgp = c(2, 0.7, 0))  # Adjust label position
-plot(x_values, SMSE_p0.2 , type = "l", col = colors[1], lty = 1, lwd = 2.5, 
-     ylim =  c(0 , 4), 
+plot(x_values, SMSE_p0.2 , type = "l", col = colors[1], lty = 1, lwd = 2.5,
+     ylim =  c(0 , 4),
      xlab = "tail fraction k/n",
-     ylab = "" , #  "ED-S", 
-     cex.lab = 1.5, cex.axis = 1.3, 
+     ylab = "" , #  "ED-S",
+     cex.lab = 1.5, cex.axis = 1.3,
      xaxt = "n")
 
 # Add custom x-axis ticks and labels
@@ -199,21 +199,21 @@ points(x_values, SMSE_p1 , col = colors[4], pch = 16, cex = 1.5)
 
 
 
-#######Extreme directions identification 
+#######Extreme directions identification
 
 
 
-############################Ectreme direction identification 
+############################Ectreme direction identification
 
 n <- 100
-count <- 0 
-list <- list() 
-for(i in 1: n){         
+count <- 0
+list <- list()
+for(i in 1: n){
   file_path <- paste0("C:/Users/mourahib/Desktop/github/Penalized_least-squares_estimator/Results/DA_mix_HR/extr_dir_ident/kn0.08/result_" , i, ".rds")
   if(file.exists(file_path)){
     count <- count + 1
     result <- readRDS(file_path)
-    list[[count]] <- result 
+    list[[count]] <- result
   }
   else(print(i))
 }
@@ -245,7 +245,7 @@ for( j in 1:10  ){
       list_matrices[[i]] <- list[[i]][[j]]$Estimation$pls_matrix
       scorei <- EDS( list_matrices[[i]] , A)
       if(scorei>1){print(3)}
-      EDscore_kn0.08[j] <- EDscore_kn0.08[j] + scorei/n 
+      EDscore_kn0.08[j] <- EDscore_kn0.08[j] + scorei/n
     }
     else(print(i))
   }
@@ -259,11 +259,11 @@ x_values <- seq(500 , 5000 , by = 500)
 
 # Set up an empty plot with appropriate axis limits
 par(mgp = c(2, 0.7, 0))  # Adjust label position
-plot(x_values, EDscore_kn0.04 , type = "l", col = colors[1], lty = 1, lwd = 2.5, 
-     ylim =  c(0.6 , 1), 
-     xlab =  "n" , 
-     ylab = "", 
-     cex.lab = 1.5, cex.axis = 1.3, 
+plot(x_values, EDscore_kn0.04 , type = "l", col = colors[1], lty = 1, lwd = 2.5,
+     ylim =  c(0.6 , 1),
+     xlab =  "n" ,
+     ylab = "",
+     cex.lab = 1.5, cex.axis = 1.3,
      xaxt = "n")
 
 # Add custom x-axis ticks and labels
@@ -308,7 +308,7 @@ bp <- boxplot(mat,
         main = "Sample size N = 3000",
         xlab = "Elements",
         ylab = "Values",
-        names = labels, 
+        names = labels,
         ylim= c(0,5))
 for (i in 1:length(bp$names)) {
   # You can tweak the width (here: 0.3 on each side of the box center)
@@ -316,7 +316,7 @@ for (i in 1:length(bp$names)) {
 }
 
 
-###########Effect of the penaliation exponent 
+###########Effect of the penaliation exponent
 
 #Calculus of SMSE2 score
 list <- readRDS(file= paste0("C:/Users/mourahib/Desktop/github/Penalized_least-squares_estimator/Results/DA_mix_log/effect_alpha/N3000/kn0.6.rds"))
@@ -326,7 +326,7 @@ for(j in 1 : 10){
   for( i in 1: n){
     list_estim[[i]] <- list[[i]][[j]]$Estimation
   }
-  SMSE_kn0.6[j] <- SMSE_log_2(list_estim , A , dep = alpha )
+  SMSE_kn0.6[j] <- SMSE_log(list_estim , A , dep = alpha )
 }
 
 #####plot SMSE
@@ -334,16 +334,16 @@ for(j in 1 : 10){
 colors <- c("#E69F00", "#56B4E9", "#009E73")  # Orange, Blue, Green
 
 # X-axis values
-x_values <- seq(0.1 , 1 , by=0.1) 
+x_values <- seq(0.1 , 1 , by=0.1)
 
 # Set up an empty plot with appropriate axis limits
 par(mgp = c(2, 0.7, 0))  # Adjust label position
-plot(x_values, SMSE_kn0.4 , type = "l", col = colors[1], lty = 1, lwd = 2.5, 
-     ylim =  c(0 , 1.2),    
+plot(x_values, SMSE_kn0.4 , type = "l", col = colors[1], lty = 1, lwd = 2.5,
+     ylim =  c(0 , 1.2),
      xlab = expression(p)
      , ylab = "" #"SMSE"
-     , 
-     cex.lab = 1.5, cex.axis = 1.3, 
+     ,
+     cex.lab = 1.5, cex.axis = 1.3,
      xaxt = "n")
 
 # Add custom x-axis ticks and labels
@@ -416,23 +416,23 @@ head(u)
 lambda_grid <- seq(0.01, 1 , length.out = 50)
 p <- 0.4
 k <- nrow(sim_data)/10
-num_class <- 5  
+num_class <- 5
 points_log <- c(0,1/4 , 1/3 , 1/2 , 3/4 ,1)
 Grid_points_log <- selectGrid(cst = points_log, d = 3, nonzero  = c( 2,3) )
 
 cl <- makeCluster(6)
 # Export necessary functions and variables to cluster
 clusterExport(cl, varlist = c(   "main_application", "shuffleCols" ,"param_estim_application", "construct_symmetric_matrix",  "normalize_group", "cross_validation_application", "p", "k","Grid_points_log" , "X"  , "num_class"
-                                 , "lambda_grid" 
+                                 , "lambda_grid"
 ))
 clusterEvalQ(cl, { dyn.load("main.dll") })
 
-set.seed(123) 
+set.seed(123)
 
 
 
 
 
-res <- main_oversteps_application( data = u , lambda_grid , grid = Grid_points_log,  start = NULL , 
+res <- main_oversteps_application( data = u , lambda_grid , grid = Grid_points_log,  start = NULL ,
                                    type = "SSR_row_log", k, p, num_class , cl )
 
