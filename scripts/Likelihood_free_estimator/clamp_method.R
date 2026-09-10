51865f3e63cb655dc7408740eff4653a2b3fdc59
AnasMourahib <anas.mourahib93@gmail.com>
2026-09-10 16:55:32 +0200

    I have deleted all files now, the old version is in the archive branch
    I have deleted all files now, the old version is in the archive branch

--- scripts/Likelihood_free_estimator/clamp_method.R
+++
@@ -1,6 +0,0 @@
-
-# Include your Julia file
-juliaEval('include("C:/Users/20254817/Desktop/Githib/Penalized_least-squares_estimator/scripts/Likelihood_free_estimator/deepset_model.jl")')
-
-# Access the estimator object
-estimator <- juliaEval("estimator")
